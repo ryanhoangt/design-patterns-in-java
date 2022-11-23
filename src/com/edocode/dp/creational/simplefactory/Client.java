2aaba3d364c5524687226d33bf50912075be8b9f
Edo <descience.thh10@gmail.com>
2022-11-23 12:26:13 +0700

    add Simple Factory pattern

---
+++ src/com/edocode/dp/creational/simplefactory/Client.java
@@ -0,0 +1,11 @@
+package com.edocode.dp.creational.simplefactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Post post = PostFactory.createPost("blog");
+		System.out.println(post);
+
+	}
+
+}
