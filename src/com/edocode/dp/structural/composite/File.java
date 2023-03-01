01bbd3aef022b67ff7d851be338cf3b21d99b674
Edo <descience.thh10@gmail.com>
2023-03-01 15:27:06 +0700

    add Composite pattern

---
+++ src/com/edocode/dp/structural/composite/File.java
@@ -0,0 +1,31 @@
+package com.edocode.dp.structural.composite;
+
+//The component base class for composite pattern
+//defines operations applicable both leaf & composite 
+public abstract class File {
+
+	private String name;
+
+	public File(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// The following methods can be removed from files and
+	// exists only on 'Directory' class. Both are valid.
+	public abstract void ls();
+	
+	public abstract void addFile(File file);
+
+	public abstract File[] getFiles();
+	
+	public abstract boolean removeFile(File file);
+	
+}
